829b33cc56be7137ca6c94d3dbe54c949b849858
Martí Bosch <marti.bosch.1992@gmail.com>
2017-05-05 15:28:25 +0200

    changed templatetag name

--- wagtail_modeltranslation/templatetags/wagtail_modeltranslation.py
+++ wagtail_modeltranslation/templatetags/wagtail_modeltranslation.py
@@ -14,18 +14,21 @@ register = template.Library()
 
 # CHANGE LANGUAGE
 @register.simple_tag(takes_context=True)
-def change_lang(context, lang=None, *args, **kwargs):
+def translated_url(context, lang=None, *args, **kwargs):
     current_language = get_language()
 
     if 'request' in context and lang and current_language:
         request = context['request']
         match = resolve(request.path)
-        non_prefixed_path = re.sub(current_language + '/', '', request.path, count=1)
+        non_prefixed_path = re.sub(
+            current_language + '/', '', request.path, count=1)
 
         # means that is an wagtail page object
         if match.url_name == 'wagtail_serve':
-            path_components = [component for component in non_prefixed_path.split('/') if component]
-            page, args, kwargs = request.site.root_page.specific.route(request, path_components)
+            path_components = [
+                component for component in non_prefixed_path.split('/') if component]
+            page, args, kwargs = request.site.root_page.specific.route(
+                request, path_components)
 
             activate(lang)
             translated_url = page.url
@@ -33,7 +36,8 @@ def change_lang(context, lang=None, *args, **kwargs):
 
             return translated_url
         elif match.url_name == 'wagtailsearch_search':
-            path_components = [component for component in non_prefixed_path.split('/') if component]
+            path_components = [
+                component for component in non_prefixed_path.split('/') if component]
 
             translated_url = '/' + lang + '/' + path_components[0] + '/'
             if request.GET:
