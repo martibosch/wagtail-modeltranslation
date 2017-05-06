ea76c2a0dbdf0a290fc4aac8c6d364420a77f399
Martí Bosch <marti.bosch.1992@gmail.com>
2017-05-06 16:00:25 +0200

    prevented route patch for RoutablePageMixin

--- wagtail_modeltranslation/patch_wagtailadmin.py
+++ wagtail_modeltranslation/patch_wagtailadmin.py
@@ -13,6 +13,7 @@ from modeltranslation.translator import translator, NotRegistered
 from modeltranslation.utils import build_localized_fieldname, get_language
 from wagtail.contrib.settings.models import BaseSetting
 from wagtail.contrib.settings.views import get_setting_edit_handler
+from wagtail.contrib.wagtailroutablepage.models import RoutablePageMixin
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, \
     MultiFieldPanel, FieldRowPanel, InlinePanel, StreamFieldPanel
 from wagtail.wagtailcore.models import Page, Site
@@ -27,8 +28,10 @@ from wagtail_modeltranslation.utils import compare_class_tree_depth
 
 logger = logging.getLogger('wagtail.core')
 
-SIMPLE_PANEL_CLASSES = [FieldPanel, ImageChooserPanel, StreamFieldPanel] + CUSTOM_SIMPLE_PANELS
-COMPOSED_PANEL_CLASSES = [MultiFieldPanel, FieldRowPanel] + CUSTOM_COMPOSED_PANELS
+SIMPLE_PANEL_CLASSES = [FieldPanel, ImageChooserPanel,
+                        StreamFieldPanel] + CUSTOM_SIMPLE_PANELS
+COMPOSED_PANEL_CLASSES = [MultiFieldPanel,
+                          FieldRowPanel] + CUSTOM_COMPOSED_PANELS
 
 
 class WagtailTranslator(object):
@@ -67,7 +70,8 @@ class WagtailTranslator(object):
             if hasattr(model, 'promote_panels'):
                 model.promote_panels = self._patch_panels(model.promote_panels)
             if hasattr(model, 'settings_panels'):
-                model.settings_panels = self._patch_panels(model.settings_panels)
+                model.settings_panels = self._patch_panels(
+                    model.settings_panels)
 
         # Clear the edit handler cached value, if it exists, so wagtail reconstructs
         # the edit_handler based on the patched panels
@@ -75,23 +79,29 @@ class WagtailTranslator(object):
 
         # SEARCH FIELDS PATCHING
 
-        translation_registered_fields = translator.get_options_for_model(model).fields
+        translation_registered_fields = translator.get_options_for_model(
+            model).fields
 
         for field in model.search_fields:
-            # Check if the field is a SearchField and if it is one of the fields registered for translation
+            # Check if the field is a SearchField and if it is one of the
+            # fields registered for translation
             if field.__class__ is SearchField and field.field_name in translation_registered_fields:
                 # If it is we create a clone of the original SearchField to keep all the defined options
                 # and replace its name by the translated one
                 for language in mt_settings.AVAILABLE_LANGUAGES:
                     translated_field = copy.deepcopy(field)
-                    translated_field.field_name = build_localized_fieldname(field.field_name, language)
-                    model.search_fields = list(model.search_fields) + [translated_field]
+                    translated_field.field_name = build_localized_fieldname(
+                        field.field_name, language)
+                    model.search_fields = list(
+                        model.search_fields) + [translated_field]
 
         # OVERRIDE PAGE METHODS
 
         model.move = _new_move
         model.set_url_path = _new_set_url_path
-        model.route = _new_route
+        if RoutablePageMixin not in model.__bases__:
+            model.route = _new_route
+
         model.get_site_root_paths = _new_get_site_root_paths
         model.relative_url = _new_relative_url
         model.url = _new_url
@@ -120,9 +130,11 @@ class WagtailTranslator(object):
 
         for panel in panels_list:
             if panel.__class__ in SIMPLE_PANEL_CLASSES:
-                patched_panels += self._patch_simple_panel(current_patching_model, panel)
+                patched_panels += self._patch_simple_panel(
+                    current_patching_model, panel)
             elif panel.__class__ in COMPOSED_PANEL_CLASSES:
-                patched_panels.append(self._patch_composed_panel(panel, related_model))
+                patched_panels.append(
+                    self._patch_composed_panel(panel, related_model))
             elif panel.__class__ == InlinePanel:
                 patched_panels.append(self._patch_inline_panel(panel))
             else:
@@ -133,7 +145,8 @@ class WagtailTranslator(object):
     def _patch_simple_panel(self, model, original_panel):
         panel_class = original_panel.__class__
         translated_panels = []
-        translation_registered_fields = translator.get_options_for_model(model).fields
+        translation_registered_fields = translator.get_options_for_model(
+            model).fields
 
         # If the panel field is not registered for translation
         # the original one is returned
@@ -142,7 +155,8 @@ class WagtailTranslator(object):
 
         for language in mt_settings.AVAILABLE_LANGUAGES:
             original_field = model._meta.get_field(original_panel.field_name)
-            localized_field_name = build_localized_fieldname(original_panel.field_name, language)
+            localized_field_name = build_localized_fieldname(
+                original_panel.field_name, language)
 
             # if the original field is required and the current language is the default one
             # this field's blank property is set to False
@@ -164,7 +178,8 @@ class WagtailTranslator(object):
 
     def _patch_composed_panel(self, original_panel, related_model=None):
         panel_class = original_panel.__class__
-        patched_children_panels = self._patch_panels(original_panel.children, related_model)
+        patched_children_panels = self._patch_panels(
+            original_panel.children, related_model)
 
         localized_panel = panel_class(patched_children_panels)
 
@@ -194,7 +209,8 @@ class WagtailTranslator(object):
         except NotRegistered:
             pass
         else:
-            related_model.panels = self._patch_panels(getattr(related_model, 'panels', []), related_model)
+            related_model.panels = self._patch_panels(
+                getattr(related_model, 'panels', []), related_model)
 
         # The original panel is returned as only the related_model panels need to be
         # patched, leaving the original untouched
@@ -212,14 +228,16 @@ def _new_move(self, target, pos=None):
     super(Page, self).move(target, pos=pos)
     # treebeard's move method doesn't actually update the in-memory instance, so we need to work
     # with a freshly loaded one now
-    # added .specific to use the most specific class so that url_paths are updated to all languages
+    # added .specific to use the most specific class so that url_paths are
+    # updated to all languages
     new_self = Page.objects.get(id=self.id).specific
     new_url_path = new_self.set_url_path(new_self.get_parent())
     new_self.save()
     new_self._update_descendant_url_paths(old_url_path, new_url_path)
 
     # Log
-    logger.info("Page moved: \"%s\" id=%d path=%s", self.title, self.id, new_url_path)
+    logger.info("Page moved: \"%s\" id=%d path=%s",
+                self.title, self.id, new_url_path)
 
 
 def _new_set_url_path(self, parent):
@@ -230,9 +248,12 @@ def _new_set_url_path(self, parent):
     """
     for language in mt_settings.AVAILABLE_LANGUAGES:
         localized_slug_field = build_localized_fieldname('slug', language)
-        default_localized_slug_field = build_localized_fieldname('slug', mt_settings.DEFAULT_LANGUAGE)
-        localized_url_path_field = build_localized_fieldname('url_path', language)
-        default_localized_url_path_field = build_localized_fieldname('url_path', mt_settings.DEFAULT_LANGUAGE)
+        default_localized_slug_field = build_localized_fieldname(
+            'slug', mt_settings.DEFAULT_LANGUAGE)
+        localized_url_path_field = build_localized_fieldname(
+            'url_path', language)
+        default_localized_url_path_field = build_localized_fieldname(
+            'url_path', mt_settings.DEFAULT_LANGUAGE)
 
         if parent:
             parent = parent.specific
@@ -240,11 +261,13 @@ def _new_set_url_path(self, parent):
             # Emulate the default behavior of django-modeltranslation to get the slug and url path
             # for the current language. If the value for the current language is invalid we get the one
             # for the default fallback language
-            slug = getattr(self, localized_slug_field, None) or getattr(self, default_localized_slug_field, self.slug)
+            slug = getattr(self, localized_slug_field, None) or getattr(
+                self, default_localized_slug_field, self.slug)
             parent_url_path = getattr(parent, localized_url_path_field, None) or \
-                              getattr(parent, default_localized_url_path_field, parent.url_path)
+                getattr(parent, default_localized_url_path_field, parent.url_path)
 
-            setattr(self, localized_url_path_field, parent_url_path + slug + '/')
+            setattr(self, localized_url_path_field,
+                    parent_url_path + slug + '/')
 
         else:
             # a page without a parent is the tree root,
@@ -296,7 +319,7 @@ def _new_get_site_root_paths():
     result = [
         (site.id, site.root_page.specific.url_path, site.root_url)
         for site in Site.objects.select_related('root_page').order_by('-root_page__url_path')
-        ]
+    ]
 
     return result
 
@@ -365,7 +388,8 @@ def _validate_slugs(page):
         siblings_slugs = [sibling.slug for sibling in siblings]
 
         if page.specific.slug in siblings_slugs:
-            errors[build_localized_fieldname('slug', language)] = _("This slug is already in use")
+            errors[build_localized_fieldname('slug', language)] = _(
+                "This slug is already in use")
 
     # Re-enable the original language
     trans_real.activate(current_language)
@@ -389,7 +413,8 @@ def _patch_clean(model):
 
 
 def patch_wagtail_models():
-    # After all models being registered the Page or BaseSetting subclasses and snippets are patched
+    # After all models being registered the Page or BaseSetting subclasses and
+    # snippets are patched
     registered_models = translator.get_registered_models()
 
     # We need to sort the models to ensure that subclasses of a model are registered first,
